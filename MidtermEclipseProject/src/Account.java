8e00eed886122739bffee5793bbd4552e10d1e13
Yasin Shuman <yshuman@loyalloops.com>
2017-02-17 17:27:52 -0800

    added prelim classes and methods

--- MidtermEclipseProject/src/Account.java
+++ MidtermEclipseProject/src/Account.java
@@ -1,4 +1,36 @@
+import java.text.NumberFormat;
 
-public class Account implements Bankable {
+public class Account implements Bankable{
+	double balance = 1000.00;
+	NumberFormat cash = NumberFormat.getCurrencyInstance();
+	@Override
+	public void withdrawal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deposit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calcInterest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calcFees() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayBalance() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- MidtermEclipseProject/src/AccountApplication.java
+++ MidtermEclipseProject/src/AccountApplication.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class AccountApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Scanner sc = new Scanner(System.in);
+		Savings s = new Savings();
+		Checking c = new Checking();
+		
+		System.out.println("Welcome to the Account application\n\n" +
+				"Starting Balances\nChecking: " + s.cash.format(s.balance) + 
+				"\nSavings: " + c.cash.format(c.balance));
+		System.out.println("\nEnter the transactions for this month\n");
+		
+		Validator.getChoiceString(sc, "Withdrawal or deposit? (w/d): " , "w", "d");
+		
 }
+	}
