8b345a4e48e810e1b37219499940084870e33c96
Yasin Shuman <yshuman@loyalloops.com>
2017-02-18 17:21:25 -0800

    added method for transactions

--- MidtermEclipseProject/src/Account.java
+++ MidtermEclipseProject/src/Account.java
@@ -2,19 +2,25 @@
 public abstract class Account implements Bankable{
 	double balance = 1000.00;
 	double amount;
-	final double INTEREST = .01;
-	final double FEES = 1.00;
-	public double withdrawal(Account a, double i) {
+	public double withdraw(double i) {
 		amount = i;
 		return balance -= amount;
 		
 	}
-	public double deposit(Account a, double i) {
+	public double deposit(double i) {
 		amount = i;
 		return balance += amount;
-				
 	}
 	public double calcInterest() {
-	return 0;
+		return 0;
 	}	
+	public double transact(String trans, double amount) {
+		if (trans.equalsIgnoreCase("w")){
+			withdraw(amount);
+		} else {
+			deposit(amount);
+		}
+		
+		return balance;
+	}
 }
